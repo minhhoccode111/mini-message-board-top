0989cb663e50ffb35a4ba762c8e325537466f6d6
Đặng Hoàng Minh <minhhoccode111@gmail.com>
2024-01-18 11:36:29 +0700

    Create and Read of this project

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+// Factory function to create a new chat
+const Chat = (name, text) => ({ name, text, added: new Date() });
+
+const messages = [
+  {
+    name: 'Mario',
+    text: 'Hi there!',
+    added: new Date(),
+  },
+  {
+    name: 'Maria',
+    text: 'Hello, World!',
+    added: new Date(),
+  },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Mini message board', messages });
+});
+
+router.post('/new', (req, res, next) => {
+  const body = req.body;
+
+  const newChat = Chat(body.username, body.message);
+
+  console.log(newChat);
+
+  messages.push(newChat);
+
+  res.redirect('/');
 });
 
 module.exports = router;
